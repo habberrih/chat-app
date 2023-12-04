0786ad89939ed0a74fdaaf034b2ff4868319752f
habberrih <abdul2013ly@gmail.com>
2023-12-04 14:23:55 +0200

    refactor: add buttoms bar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,25 +1,70 @@
+import 'package:chat_app/config/app_icons.dart';
+import 'package:chat_app/config/app_strings.dart';
+import 'package:chat_app/pages/home_page.dart';
+import 'package:chat_app/pages/profile/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Buttom Navigation Container'),
-      ),
-      body: Center(child: Text('Center Text')),
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: const [ //! const
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: SvgPicture.asset(AppIcons.icHome),
+            label: AppStrings.home,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
+            icon: SvgPicture.asset(AppIcons.icFavorite),
+            label: AppStrings.favorites,
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.icAdd),
+            label: AppStrings.add,
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.icMessage),
+            label: AppStrings.messages,
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AppIcons.icUser),
+            label: AppStrings.user,
           ),
         ],
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
       ),
     );
   }
+
+  final pages = [
+    HomePage(),
+    Center(
+      child: Text('Favorite'),
+    ),
+    Center(
+      child: Text('Add Post'),
+    ),
+    Center(
+      child: Text('Messages'),
+    ),
+    ProfilePage(),
+  ];
 }
