d3c7311239985d8e6153d8318ded8faa851e8cfc
habberrih <abdul2013ly@gmail.com>
2023-12-05 00:19:34 +0200

    refactor: change to underline border

--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -11,7 +11,17 @@ class AppTextField extends StatelessWidget {
     return TextField(
       decoration: InputDecoration(
         hintText: hint,
-        border: const OutlineInputBorder(
+        labelText: hint,
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        border: const UnderlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(12),
+          ),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
           borderRadius: BorderRadius.all(
             Radius.circular(12),
           ),
