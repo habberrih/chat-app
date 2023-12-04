2f7ac6bfed0e35f40b3e06966c9b291f07460101
habberrih <abdul2013ly@gmail.com>
2023-12-04 14:22:52 +0200

    refactor: moved routes to cusom class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:chat_app/pages/home_page.dart';
-import 'package:chat_app/pages/login_page.dart';
-import 'package:chat_app/pages/main_page.dart';
-import 'package:chat_app/styles/app_colors.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+
+import 'styles/app_colors.dart';
+import 'config/app_routes.dart';
 
 void main() {
   runApp(DevicePreview(
@@ -21,15 +20,12 @@ class HomeApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Urbanist',
         scaffoldBackgroundColor: AppColors.background,
+        brightness: Brightness.dark,
       ),
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const LoginPage(),
-        '/home': (context) => const HomePage(),
-        '/main': (context) => const MainPage(),
-      },
+      initialRoute: AppRoutes.login,
+      routes: AppRoutes.pages,
     );
   }
 }
