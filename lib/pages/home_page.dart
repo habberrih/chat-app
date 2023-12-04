a4d9dc38b33cfaef62863b00933cc3c45a52656b
habberrih <abdul2013ly@gmail.com>
2023-12-04 08:57:25 +0200

    refactor: add function to get users

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/styles/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,15 +11,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       appBar: AppBar(
+        backgroundColor: AppColors.background,
         title: const Text('5minuteflutter'),
-        centerTitle: true,
+        centerTitle: false,
         actions: [
           Icon(Icons.location_on_outlined),
         ],
       ),
-      body: Container(),
+      body: ListView(
+        children: mockUsersFromServer(),
+      ),
     );
   }
+}
+
+Widget _userItem() {
+  return Row(
+      children: [
+        Image.asset(
+          'assets/temp/me.jpeg',
+          width: 48,
+          height: 48,
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        Text('Abdullah Habberrih'),
+      ],
+    );
+}
+
+List<Widget> mockUsersFromServer() {
+  List<Widget> users = [];
+  for(var i = 0; i< 500; i++){
+    users.add(_userItem());
+  }
+  return users; 
 }
