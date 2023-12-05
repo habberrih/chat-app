4d7dcb2fb0f7511298ae5b71496ee9c4c134b0e9
habberrih <abdul2013ly@gmail.com>
2023-12-05 19:17:08 +0200

    refactor: replace hard string with vars

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,17 +1,20 @@
 import '../pages/home_page.dart';
 import '../pages/main_page.dart';
+import '../pages/nearby_page.dart';
 import '../pages/profile/edit_profile_page.dart';
 import '../pages/login_page.dart';
 
 class AppRoutes {
   static final pages = {
-    '/': (context) => const LoginPage(),
-    '/home': (context) => const HomePage(),
-    '/main': (context) => const MainPage(),
-    '/edit_profile': (context) => const EditProfilePage(),
+    login: (context) => const LoginPage(),
+    home: (context) => const HomePage(),
+    main: (context) => const MainPage(),
+    editProfile: (context) => const EditProfilePage(),
+    nearby: (context) => const NearbyPage(),
   };
   static const login = '/';
   static const home = '/home';
   static const main = '/main';
   static const editProfile = '/edit_profile';
+  static const nearby = '/nearby';
 }
