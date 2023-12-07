db6a8e7e319530cc8d427f998cd97d3eb863f67e
habberrih <abdul2013ly@gmail.com>
2023-12-07 19:38:02 +0200

    feat: add Login response

---
+++ lib/data/responses/login_response.dart
@@ -0,0 +1,13 @@
+import '../../models/user.dart';
+
+class LoginResponse {
+  final User user;
+  final String token;
+
+  LoginResponse({required this.user, required this.token});
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
+        user: User.fromJson(json),
+        token: json['token'],
+      );
+}
