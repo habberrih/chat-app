3550106664ab676639c7f485cd51c2292284624f
habberrih <abdul2013ly@gmail.com>
2023-12-09 18:56:11 +0200

    feat: get all posts provider

---
+++ lib/provider/post_provider.dart
@@ -0,0 +1,13 @@
+import '../data/services/get_post_service.dart';
+import 'package:flutter/material.dart';
+
+import '../data/models/post.dart';
+
+class PostProvider extends ChangeNotifier {
+  final List<Post> list = [];
+
+  getPost() async {
+    list.clear();
+    list.addAll(await GetPostService().call());
+  }
+}
